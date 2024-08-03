24d10c1746823af360f55d613db15ab6d1995784
ZedongPeng <peng_zedong@126.com>
2024-08-03 17:18:01 -0400

    update model size table

--- generate_model_size_report.py
+++ generate_model_size_report.py
@@ -6,20 +6,22 @@ import pandas as pd
 
 if __name__ == "__main__":
     instance_list = [
-        # "batch_processing",
-        # "biofuel",
-        # "disease_model",
-        # "gdp_col",
-        # "hda",
+        "batch_processing",
+        # "biofuel", # issue 31
+        "cstr",
+        "disease_model",
+        "gdp_col",
+        "hda",
         "jobshop",
-        # "kaibel",
-        # "logical",
-        # "med_term_purchasing",
-        # "methanol",
-        # "mod_hens",
-        # "modprodnet",
-        # "stranded_gas",
-        # "syngas",
+        # "kaibel", # next step
+        "med_term_purchasing",
+        "methanol",
+        # "mod_hens", # next step
+        "modprodnet",
+        "positioning",
+        "spectralog",
+        "stranded_gas",
+        "syngas",
     ]
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     timelimit = 600
@@ -30,7 +32,19 @@ if __name__ == "__main__":
         model = import_module("gdplib." + instance).build_model()
         report = build_model_size_report(model)
         report_df = pd.DataFrame(report.overall, index=[0]).T
-        report_df.index.name = "Component"
+
         report_df.columns = ["Number"]
+        report_df.index = [
+            "Variables",
+            "Binary variables",
+            "Integer variables",
+            "Continuous variables",
+            "Disjunctions",
+            "Disjuncts",
+            "Constraints",
+            "Nonlinear constraints",
+        ]
+        report_df.index.name = "Component"
+
         # Generate the model size report (Markdown)
         report_df.to_markdown("gdplib/" + instance + "/" + "model_size_report.md")
