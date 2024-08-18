a433ab3a3eee77aba6483a772ba3b75bfebb528a
ZedongPeng <peng_zedong@126.com>
2024-08-18 15:51:22 -0400

    update generate_model_size_report.py to support different cases

--- generate_model_size_report.py
+++ generate_model_size_report.py
@@ -5,35 +5,53 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    instance_list = [
-        "batch_processing",
-        "biofuel",
-        "cstr",
-        "disease_model",
-        "gdp_col",
-        "hda",
-        "jobshop",
+    instance_dict = {
+        "batch_processing": [],
+        "biofuel": [],
+        "cstr": [],
+        "disease_model": [],
+        "gdp_col": [],
+        "hda": [],
+        "jobshop": [],
         # "kaibel", # next step
-        "med_term_purchasing",
-        "methanol",
-        # "mod_hens", # next step
-        "modprodnet",
-        "positioning",
-        "spectralog",
-        "stranded_gas",
-        "syngas",
-    ]
+        "med_term_purchasing": [],
+        "methanol": [],
+        "mod_hens": [
+            "conventional",
+            "single_module_integer",
+            "multiple_module_integer",
+            "mixed_integer",
+            "single_module_discrete",
+            "multiple_module_discrete",
+            "mixed_discrete",
+        ],
+        "modprodnet": [],
+        "positioning": [],
+        "spectralog": [],
+        "stranded_gas": [],
+        "syngas": [],
+    }
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     timelimit = 600
 
-    for instance in instance_list:
+    for instance, cases in instance_dict.items():
         print("Generating model size report: " + instance)
 
-        model = import_module("gdplib." + instance).build_model()
-        report = build_model_size_report(model)
-        report_df = pd.DataFrame(report.overall, index=[0]).T
+        if cases == []:
+            model = import_module("gdplib." + instance).build_model()
+            report = build_model_size_report(model)
+            report_df = pd.DataFrame(report.overall, index=[0]).T
 
-        report_df.columns = ["Number"]
+            report_df.columns = ["Number"]
+        else:
+            report_df = pd.DataFrame()
+            for case in cases:
+                model = import_module("gdplib." + instance).build_model(case)
+                case_report = build_model_size_report(model)
+                temp_df = pd.DataFrame(case_report.overall, index=[0]).T
+                temp_df.columns = [case]
+                report_df = pd.concat([report_df, temp_df], axis=1)
+
         report_df.index = [
             "Variables",
             "Binary variables",
