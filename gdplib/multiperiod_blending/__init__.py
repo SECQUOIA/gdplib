4d1b75bd393bf762fa6a59ba99f53431c290ee2f
Arsh Bhatia <arshbhatia2001@gmail.com>
2026-04-07 14:46:01 -0400

    Adding convert_json_to_dict to multiperiod __init__

--- gdplib/multiperiod_blending/__init__.py
+++ gdplib/multiperiod_blending/__init__.py
@@ -1,3 +1,3 @@
-from .multiperiod_blending import build_model
+from .multiperiod_blending import build_model, convert_json_to_data
 
-__all__ = ["build_model"]
+__all__ = ["build_model", "convert_json_to_dict"]
